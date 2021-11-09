9fb711217e8c9bac5f741324e4bca2db378853d7
Ainozu <ainozuuu@gmail.com>
2021-11-09 17:45:09 +0100

    1.1.7.1
    small fix for level names

--- src/essentials/Breakout.java
+++ src/essentials/Breakout.java
@@ -11,7 +11,7 @@ public class Breakout extends JFrame {
 
     public Breakout(String name,String level_name,ArrayList<String> lines,int[]datas) {
     	add(new Board(level_name,lines,datas));
-        setTitle(level_name);
+        setTitle(name);
 
         setLocationRelativeTo(null);
         setResizable(false);

34ba8cd079fa98d5dceb632c7a574a9482715479
Ainozu <ainozuuu@gmail.com>
2022-01-13 20:35:59 +0100

    1.2.3
    portable,runnable,idkable

--- src/essentials/Commons.java
+++ src/essentials/Commons.java
@@ -5,9 +5,9 @@ public interface Commons {
     int WIDTH = 900;
     int HEIGHT = 800;
     int BOTTOM_EDGE = 690;
-    int INIT_PADDLE_X = 200;
+    int INIT_PADDLE_X = 450;
     int INIT_PADDLE_Y = 660;
-    int INIT_BALL_X = 230;
+    int INIT_BALL_X = 450;
     int INIT_BALL_Y = 355;
     int PERIOD = 10;
 }
